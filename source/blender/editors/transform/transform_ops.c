ca336c600b86b93bca7d0f78c25cbe40106c10ae
jon denning <gfxcoder@gmail.com>
2022-06-07 16:25:15 -0400

    revert transform API changes (moved to another patch)

--- source/blender/editors/transform/transform_ops.c
+++ source/blender/editors/transform/transform_ops.c
@@ -566,17 +566,6 @@ static bool transform_poll_property(const bContext *UNUSED(C),
     }
   }
 
-  /* Snapping. */
-  {
-    PropertyRNA *prop_snap = RNA_struct_find_property(op->ptr, "snap");
-    if (prop_snap && (prop_snap != prop) &&
-        (RNA_property_boolean_get(op->ptr, prop_snap) == false)) {
-      if (STRPREFIX(prop_id, "snap") || STRPREFIX(prop_id, "use_snap")) {
-        return false;
-      }
-    }
-  }
-
   return true;
 }
 
@@ -659,17 +648,11 @@ void Transform_Properties(struct wmOperatorType *ot, int flags)
     prop = RNA_def_boolean(ot->srna, "snap", 0, "Use Snapping Options", "");
     RNA_def_property_flag(prop, PROP_HIDDEN);
 
-    prop = RNA_def_enum(
-        ot->srna, "snap_elements", rna_enum_snap_element_items, 0, "Snap to Elements", "");
-    RNA_def_property_flag(prop, PROP_ENUM_FLAG);
-
-    RNA_def_boolean(ot->srna, "use_snap_project", false, "Project Individual Elements", "");
-
     if (flags & P_GEO_SNAP) {
-      /* TODO(@gfxcoder): Rename `snap_target` to `snap_source` to avoid previous ambiguity of
-       * "target" (now, "source" is geometry to be moved and "target" is geometry to which moved
-       * geometry is snapped).  Use "Source snap point" and "Point on source that will snap to
-       * target" for name and description, respectively. */
+      /* TODO(@gfxcoder): Rename `snap_target` to `snap_source` to avoid
+       * previous ambiguity of "target" (now, "source" is geometry to be moved and "target" is
+       * geometry to which moved geometry is snapped).  Use "Source snap point" and "Point on
+       * source that will snap to target" for name and description, respectively. */
       prop = RNA_def_enum(ot->srna, "snap_target", rna_enum_snap_source_items, 0, "Target", "");
       RNA_def_property_flag(prop, PROP_HIDDEN);
       prop = RNA_def_float_vector(
@@ -677,7 +660,8 @@ void Transform_Properties(struct wmOperatorType *ot, int flags)
       RNA_def_property_flag(prop, PROP_HIDDEN);
 
       if (flags & P_ALIGN_SNAP) {
-        RNA_def_boolean(ot->srna, "snap_align", 0, "Align with Point Normal", "");
+        prop = RNA_def_boolean(ot->srna, "snap_align", 0, "Align with Point Normal", "");
+        RNA_def_property_flag(prop, PROP_HIDDEN);
         prop = RNA_def_float_vector(
             ot->srna, "snap_normal", 3, NULL, -FLT_MAX, FLT_MAX, "Normal", "", -FLT_MAX, FLT_MAX);
         RNA_def_property_flag(prop, PROP_HIDDEN);
