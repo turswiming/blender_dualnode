1027f4c186d177000447fc6cc24ea2aa0a2e8502
Ray Molenkamp <github@lazydodo.com>
2022-08-26 09:07:31 -0600

    deps: fix idiff failing in debug builds.
    We only ship the release idiff on windows
    which needs the release dll's not available
    in the blender.shared folder when doing
    a debug build.

--- build_files/cmake/platform/platform_win32.cmake
+++ build_files/cmake/platform/platform_win32.cmake
@@ -1005,5 +1005,8 @@ set(SYCL_ROOT_DIR ${LIBDIR}/dpcpp)
 # Environment variables to run precompiled executables that needed libraries.
 list(JOIN PLATFORM_BUNDLED_LIBRARY_DIRS ";" _library_paths)
 set(PLATFORM_ENV_BUILD "PATH=${LIBDIR}/OpenImageIO/bin\;${LIBDIR}/boost/lib\;${LIBDIR}/openexr/bin\;${LIBDIR}/imath/bin\;${PATH}")
-set(PLATFORM_ENV_INSTALL "PATH=${CMAKE_INSTALL_PREFIX_WITH_CONFIG}/blender.shared/;$ENV{PATH}")
+# Install also needs some additional folders, since idiff requires the
+# release oiio dll's not available in the blender.shared folder when doing
+# a debug build.
+set(PLATFORM_ENV_INSTALL "PATH=${CMAKE_INSTALL_PREFIX_WITH_CONFIG}/blender.shared/;${PLATFORM_ENV_BUILD}")
 unset(_library_paths)
