019173321422c5a546df8ca7496a9280b478043d
Clément Foucault <foucault.clem@gmail.com>
2022-08-27 10:38:42 +0200

    Improve culling using inscribed sphere

--- source/blender/draw/intern/shaders/draw_visibility_comp.glsl
+++ source/blender/draw/intern/shaders/draw_visibility_comp.glsl
@@ -6,9 +6,14 @@
 
 #pragma BLENDER_REQUIRE(common_math_lib.glsl)
 #pragma BLENDER_REQUIRE(common_intersect_lib.glsl)
-#pragma BLENDER_REQUIRE(common_debug_print_lib.glsl)
 
 shared uint shared_result;
+
+void mask_visibility_bit()
+{
+  uint bit = 1u << gl_LocalInvocationID.x;
+  atomicAnd(visibility_buf[gl_WorkGroupID.x], ~bit);
+}
 
 void main()
 {
@@ -23,9 +28,19 @@ void main()
                                     bounds.bounding_corners[1].xyz,
                                     bounds.bounding_corners[2].xyz,
                                     bounds.bounding_corners[3].xyz);
-    if (intersect_view(box) == false) {
-      uint bit = 1u << gl_LocalInvocationID.x;
-      atomicAnd(visibility_buf[gl_WorkGroupID.x], ~bit);
+    Sphere bounding_sphere = Sphere(bounds.bounding_sphere.xyz, bounds.bounding_sphere.w);
+    Sphere inscribed_sphere = Sphere(bounds.bounding_sphere.xyz, bounds._inner_sphere_radius);
+
+    if (intersect_view(inscribed_sphere) == true) {
+      /* Visible. */
+    }
+    else if (intersect_view(bounding_sphere) == false) {
+      /* Not visible. */
+      mask_visibility_bit();
+    }
+    else if (intersect_view(box) == false) {
+      /* Not visible. */
+      mask_visibility_bit();
     }
   }
 }
