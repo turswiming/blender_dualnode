cb826f352938be3256113cc5043162b89a2494f9
Jeroen Bakker <jeroen@blender.org>
2022-09-30 15:50:11 +0200

    Change merge shader to not fade.

--- source/blender/gpu/shaders/sculpt_paint/sculpt_paint_image_merge_comp.glsl
+++ source/blender/gpu/shaders/sculpt_paint/sculpt_paint_image_merge_comp.glsl
@@ -3,7 +3,8 @@ void main()
   ivec2 coord_in = ivec2(gl_GlobalInvocationID.xy);
   ivec2 coord_out = coord_in;
   vec4 paint_color = imageLoad(in_paint_img, coord_in);
+  paint_color.a = 1.0;
   vec4 canvas_color = imageLoad(out_img, coord_out);
-  vec4 out_color = mix(canvas_color, paint_color, 0.01);
+  vec4 out_color = max(canvas_color, paint_color);
   imageStore(out_img, coord_out, out_color);
 }
