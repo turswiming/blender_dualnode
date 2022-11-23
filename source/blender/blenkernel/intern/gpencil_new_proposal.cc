0ac9bbc98fe231a6a9b1616c482d050816ef63dd
Falk David <falkdavid@gmx.de>
2022-11-23 12:55:02 +0100

    Move classes to header and add conversion file

---
+++ source/blender/blenkernel/intern/gpencil_new_proposal.cc
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup bke
+ */
+
+#include <algorithm>
+
+#include "BKE_curves.hh"
+#include "BKE_gpencil.h"
+
+#include "BLI_index_mask_ops.hh"
+#include "BLI_math_vec_types.hh"
+
+#include "DNA_gpencil_types.h"
+
+#include "gpencil_new_proposal.hh"
+
+namespace blender::bke {
+
+}  // namespace blender::bke
---
+++ source/blender/blenkernel/intern/gpencil_new_proposal_conversion.cc
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup bke
+ */
+
+#include "gpencil_new_proposal.hh"
+
+namespace blender::bke {
+
+} // namespace blender::bke
