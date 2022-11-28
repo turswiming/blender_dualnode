b81e6ab2f0e455110e1ab7afaedaff3185bed7bb
Miguel Pozo <pragma37@gmail.com>
2022-11-28 21:22:00 +0100

    Link to Workbench Next Task in user prefs

--- release/scripts/startup/bl_ui/space_userpref.py
+++ release/scripts/startup/bl_ui/space_userpref.py
@@ -2319,7 +2319,7 @@ class USERPREF_PT_experimental_prototypes(ExperimentalPanel, Panel):
                 ({"property": "use_sculpt_texture_paint"}, "T96225"),
                 ({"property": "use_full_frame_compositor"}, "T88150"),
                 ({"property": "enable_eevee_next"}, "T93220"),
-                ({"property": "enable_workbench_next"}, ""),
+                ({"property": "enable_workbench_next"}, "T101619"),
             ),
         )
 
