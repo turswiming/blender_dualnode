49723cca426213e091ea4057d4eb1f3f319828a4
Jeroen Bakker <jeroen@blender.org>
2022-12-02 10:29:18 +0100

    Enable OpenGL/Metal shader compilation. (was disabled for debugging).

--- source/blender/gpu/intern/gpu_shader_builder.cc
+++ source/blender/gpu/intern/gpu_shader_builder.cc
@@ -103,9 +103,9 @@ int main(int argc, const char *argv[])
   };
 
   blender::Vector<NamedBackend> backends_to_validate;
-  //backends_to_validate.append({"OpenGL", GPU_BACKEND_OPENGL});
+  backends_to_validate.append({"OpenGL", GPU_BACKEND_OPENGL});
 #ifdef WITH_METAL_BACKEND
-  //backends_to_validate.append({"Metal", GPU_BACKEND_METAL});
+  backends_to_validate.append({"Metal", GPU_BACKEND_METAL});
 #endif
 #ifdef WITH_VULKAN_BACKEND
   backends_to_validate.append({"Vulkan", GPU_BACKEND_VULKAN});
@@ -122,6 +122,9 @@ int main(int argc, const char *argv[])
     if (!builder.bake_create_infos()) {
       printf("Shader compilation failed for %s backend\n", backend.name.c_str());
       exit_code = 1;
+    }
+    else {
+      printf("%s backend shader compilation succeeded.\n", backend.name.c_str());
     }
     builder.exit();
   }
