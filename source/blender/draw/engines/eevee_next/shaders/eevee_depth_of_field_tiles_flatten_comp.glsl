f1b4dce33ee606cb76a56aca00d2463f81c401ed
Jeroen Bakker <jeroen@blender.org>
2022-12-06 08:15:20 +0100

    Remove const keyword from shader global.

--- source/blender/draw/engines/eevee_next/shaders/eevee_depth_of_field_tiles_flatten_comp.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_depth_of_field_tiles_flatten_comp.glsl
@@ -26,11 +26,7 @@ shared uint bg_min_coc;
 shared uint bg_max_coc;
 shared uint bg_min_intersectable_coc;
 
-#ifdef GPU_VULKAN
 uint dof_tile_large_coc_uint = floatBitsToUint(dof_tile_large_coc);
-#else
-const uint dof_tile_large_coc_uint = floatBitsToUint(dof_tile_large_coc);
-#endif
 
 void main()
 {
