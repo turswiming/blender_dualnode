70171c7061bd442abcf75db118fb340d70543ecf
Clément Foucault <foucault.clem@gmail.com>
2023-01-02 17:20:41 +0100

    Make fragment output closest to LOD output

--- source/blender/draw/engines/eevee_next/shaders/eevee_surf_shadow_frag.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_surf_shadow_frag.glsl
@@ -14,10 +14,13 @@
 #pragma BLENDER_REQUIRE(eevee_surf_lib.glsl)
 #pragma BLENDER_REQUIRE(eevee_nodetree_lib.glsl)
 
-void write_depth(ivec2 texel_co, int lod, ivec2 tile_co, float depth)
+void write_depth(ivec2 texel_co, const int lod, ivec2 tile_co, float depth)
 {
-  if (!all(equal((texel_co >> lod) << lod, texel_co))) {
-    /* TODO(fclem): Select the pixel the closest to the mip texel to avoid too much difference. */
+  /* We need to select the lod0 pixel closest to the lod pixel which is
+   * located at the center of a lod0 pixel quad.
+   * From the 4 possible pixels we choose the top left corner. */
+  const int lod_center_offset = (lod > 0) ? (1 << (lod - 1)) : 0;
+  if (!all(equal(((texel_co >> lod) << lod) + lod_center_offset, texel_co))) {
     return;
   }
 
@@ -39,6 +42,11 @@ void main()
   float depth = gl_FragCoord.z;
 
   write_depth(texel_co, 0, tile_co, depth);
+
+  /* We have to compensate the output pixel position being different than the input pixel's.
+   * This is only half a pixel since we chose one pixel inside the quad. */
+  depth -= DFDX_SIGN * 0.5 * dFdx(gl_FragCoord.z) + DFDY_SIGN * 0.5 * dFdy(gl_FragCoord.z);
+
   write_depth(texel_co, 1, tile_co, depth);
   write_depth(texel_co, 2, tile_co, depth);
   write_depth(texel_co, 3, tile_co, depth);
