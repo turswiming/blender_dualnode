5e557b98cf7f90980443f5fdb34ff1ec1a4e3e22
Clément Foucault <foucault.clem@gmail.com>
2023-01-23 01:11:53 +0100

    Bump to 32x32 tilemaps

--- source/blender/draw/engines/eevee_next/shaders/eevee_shadow_page_free_comp.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_shadow_page_free_comp.glsl
@@ -14,33 +14,41 @@ void main()
 {
   ShadowTileMapData tilemap_data = tilemaps_buf[gl_GlobalInvocationID.z];
 
-  int tile_index = tilemap_data.tiles_index + int(gl_LocalInvocationID.x);
+  int tile_start = tilemap_data.tiles_index;
+  for (int lod = 0; lod <= SHADOW_TILEMAP_LOD; lod++) {
+    int lod_len = SHADOW_TILEMAP_LOD0_LEN >> (lod * 2);
+    int local_tile = int(gl_LocalInvocationID.x);
+    if (local_tile < lod_len) {
+      int tile_index = tile_start + local_tile;
 
-  ShadowTileData tile = shadow_tile_unpack(tiles_buf[tile_index]);
+      ShadowTileData tile = shadow_tile_unpack(tiles_buf[tile_index]);
 
-  bool is_orphaned = !tile.is_used && tile.do_update;
-  if (is_orphaned) {
-    if (tile.is_cached) {
-      shadow_page_cache_remove(tile);
+      bool is_orphaned = !tile.is_used && tile.do_update;
+      if (is_orphaned) {
+        if (tile.is_cached) {
+          shadow_page_cache_remove(tile);
+        }
+        if (tile.is_allocated) {
+          shadow_page_free(tile);
+        }
+      }
+
+      if (tile.is_used) {
+        if (tile.is_cached) {
+          shadow_page_cache_remove(tile);
+        }
+        if (!tile.is_allocated) {
+          atomicAdd(pages_infos_buf.page_alloc_count, 1);
+        }
+      }
+      else {
+        if (tile.is_allocated) {
+          shadow_page_cache_append(tile, tile_index);
+        }
+      }
+
+      tiles_buf[tile_index] = shadow_tile_pack(tile);
     }
-    if (tile.is_allocated) {
-      shadow_page_free(tile);
-    }
+    tile_start += lod_len;
   }
-
-  if (tile.is_used) {
-    if (tile.is_cached) {
-      shadow_page_cache_remove(tile);
-    }
-    if (!tile.is_allocated) {
-      atomicAdd(pages_infos_buf.page_alloc_count, 1);
-    }
-  }
-  else {
-    if (tile.is_allocated) {
-      shadow_page_cache_append(tile, tile_index);
-    }
-  }
-
-  tiles_buf[tile_index] = shadow_tile_pack(tile);
 }
